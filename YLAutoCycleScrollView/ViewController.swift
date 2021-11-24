e020ec7ad16b5138f113eb13341f2f21cc7b6143
AlbertYuan <yuanliangios@163.com>
2021-11-24 18:17:29 +0800

    Initial Commit

---
+++ YLAutoCycleScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YLAutoCycleScrollView
+//
+//  Created by AlbertYuan on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
